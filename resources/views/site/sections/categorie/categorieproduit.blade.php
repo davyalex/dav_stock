cb576fe718852b6ec2ac444dd1bbdf86a1fbf133
davyalex <alexkouamelan96pv@gmail.com>
2024-11-12 13:42:07 +0000

    optimize

--- resources/views/site/sections/categorie/categorieproduit.blade.php
+++ resources/views/site/sections/categorie/categorieproduit.blade.php
@@ -1,22 +1,45 @@
 <ul id="faq">
-    @foreach($categories as $categorie)
+    @foreach ($categories as $categorie)
         <li>
-            @if($categorie->children->isNotEmpty())
-                <a data-bs-toggle="collapse" data-bs-parent="#faq" 
-                   href="#shop-categorie-{{ $categorie->id }}" 
-                   class="text-danger {{ $categorie->famille == $categorieSelect->famille ? 'show' : 'collapsed' }}" 
-                   aria-expanded="{{ $categorie->famille == $categorieSelect->famille ? 'true' : 'false' }}">
-                    <strong class="fs-6">{{ $categorie->name }}</strong> <i class="ion-ios-arrow-down"></i>
+            @if ($categorie->children->isNotEmpty())
+                <a data-bs-toggle="collapse" data-bs-parent="#faq" href="#shop-categorie-{{ $categorie->id }}"
+                    class="text-danger {{ $categorie->famille == $categorieSelect->famille ? 'show' : 'collapsed' }}"
+                    aria-expanded="{{ $categorie->famille == $categorieSelect->famille ? 'true' : 'false' }}">
+                    <strong class="fs-6">
+                        @if ($categorie->slug === 'bar')
+                            Boissons
+                        @elseif($categorie->slug === 'cuisine-interne')
+                            Restaurant
+                        @else
+                            {{ $categorie->name }}
+                        @endif
+                    </strong> <i class="ion-ios-arrow-down"></i>
                 </a>
-                <ul id="shop-categorie-{{ $categorie->id }}" 
+                <ul id="shop-categorie-{{ $categorie->id }}"
                     class="panel-collapse collapse {{ $categorie->famille == $categorieSelect->famille ? 'show' : '' }}">
-                    @foreach($categorie->children as $child)
-                        @include('site.sections.categorie.categorieproduit', ['categories' => [$child], 'categorieSelect' => $categorieSelect])
+                    @foreach ($categorie->children as $child)
+                        @include('site.sections.categorie.categorieproduit', [
+                            'categories' => [$child],
+                            'categorieSelect' => $categorieSelect,
+                        ])
                     @endforeach
                 </ul>
             @else
-                <a href="{{route('produit' , $categorie->id)}}" class="{{ $categorie->id == $categorieSelect->id ? 'fw-bold' : '' }}">{{ $categorie->name }}</a>
+                <a href="{{ route('produit', $categorie->id) }}"
+                    class="{{ $categorie->id == $categorieSelect->id ? 'fw-bold' : '' }}">
+                    @if ($categorie->slug === 'bar')
+                        Boissons
+                    @elseif($categorie->slug === 'cuisine-interne')
+                        Restaurant
+                    @else
+                       <span class="text-capitalize"> {{ $categorie->name }}</span>
+                    @endif
+                </a>
             @endif
+
+            
+
+        
         </li>
     @endforeach
 </ul>
