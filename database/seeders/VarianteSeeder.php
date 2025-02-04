1270c08dde07b6068876edeebb0b809dd9d2e820
davyalex <alexkouamelan96pv@gmail.com>
2025-02-04 11:49:48 +0000

    optimize

--- database/seeders/VarianteSeeder.php
+++ database/seeders/VarianteSeeder.php
@@ -14,12 +14,14 @@ class VarianteSeeder extends Seeder
     public function run(): void
     {
         //  public function run()
-    {
-        // Ajout de variantes avec des prix par défaut si souhaité
-        Variante::create(['libelle' => 'Tournée', 'slug' => 'tournée']);
-        Variante::create(['libelle' => 'Ballon', 'slug' => 'ballon']);
-        Variante::create(['libelle' => 'Verre', 'slug' => 'verre']);
-        Variante::create(['libelle' => 'Bouteille', 'slug' => 'bouteille']);
-    }
+        {
+            // Ajout de variantes avec des prix par défaut si souhaité
+            // Ajouter les variantes si elles n'existent pas déjà
+            Variante::firstOrCreate(['libelle' => 'Tournée'], ['slug' => 'tournée']);
+            Variante::firstOrCreate(['libelle' => 'Ballon'], ['slug' => 'ballon']);
+            Variante::firstOrCreate(['libelle' => 'Verre'], ['slug' => 'verre']);
+            Variante::firstOrCreate(['libelle' => 'Bouteille'], ['slug' => 'bouteille']);
+            Variante::firstOrCreate(['libelle' => 'Demi'], ['slug' => 'demi']);
+        }
     }
 }
