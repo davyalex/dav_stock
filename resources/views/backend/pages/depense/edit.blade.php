8140eefeeff8fb62de5b208640378dbb26e10c88
davyalex <alexkouamelan96pv@gmail.com>
2025-03-12 14:27:21 +0000

    optimize

--- resources/views/backend/pages/depense/edit.blade.php
+++ resources/views/backend/pages/depense/edit.blade.php
@@ -16,7 +16,7 @@
                     <div class="row">
                         <div class="col-md-8">
                             <label for="validationCustom01" class="form-label">Categorie</label>
-                            <select name="categorie_depense" class="form-control categorie-select" required>
+                            <select name="categorie" class="form-control categorie-select" required>
                                 <option disabled selected value="">Selectionner</option>
                                 @foreach ($categorie_depense as $data)
                                     <!-- Si la catégorie a des libelleDepenses, rendre l'option non cliquable -->
