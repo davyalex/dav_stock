41e3c550ff7c951248e56dfafa707fefdac97d7f
davyalex <alexkouamelan96pv@gmail.com>
2025-04-14 11:35:33 +0000

    optimize

--- resources/views/backend/pages/stock/inventaire/create.blade.php
+++ resources/views/backend/pages/stock/inventaire/create.blade.php
@@ -92,7 +92,7 @@
                 <div class="card-body"> --}}
 
 
-            @if ($inventaire_existe == false)
+            @if ($inventaire_existe == true)
                 <!-- Danger Alert -->
                 <div class="alert alert-danger alert-dismissible bg-danger text-white alert-label-icon fade show material-shadow"
                     role="alert">
