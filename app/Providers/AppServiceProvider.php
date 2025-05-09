c4b7c6673870842e2da8385aba49379eef7f4239
davyalex <alexkouamelan96pv@gmail.com>
2025-05-09 15:54:03 +0000

    optimize

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -209,44 +209,44 @@ class AppServiceProvider extends ServiceProvider
 
 
 
-        function productsNotInInventaire()
-        {
-            // ##START FONCTION POUR METTRE A JOUR LE STOCK DES PRODUITS QUI NE SONT PAS DANS L'INVENTAIRE DU MOIS
+        // function productsNotInInventaire()
+        // {
+        //     // ##START FONCTION POUR METTRE A JOUR LE STOCK DES PRODUITS QUI NE SONT PAS DANS L'INVENTAIRE DU MOIS
 
 
-            // recuperer l'inventaire du mois 
-            $inventaire_mois = Inventaire::whereYear('created_at', Carbon::now()->year)
-                ->whereMonth('created_at', Carbon::now()->month)
-                ->first();
+        //     // recuperer l'inventaire du mois 
+        //     $inventaire_mois = Inventaire::whereYear('created_at', Carbon::now()->year)
+        //         ->whereMonth('created_at', Carbon::now()->month)
+        //         ->first();
 
-            // recuperer les produits de l'inventaire du mois
-            $produits_inventaire = Produit::whereHas('inventaires', function ($query) use ($inventaire_mois) {
-                $query->where('inventaires.id', $inventaire_mois->id);
-            })->get();
-
-
-            //recuperer les produits où la categorie famille est restaurant et  bar qui ne sont pas dans l'inventaire du mois
-            $produits_not_inventaire = Produit::whereDoesntHave('inventaires', function ($query) use ($inventaire_mois) {
-                $query->where('inventaires.id', $inventaire_mois->id);
-            })->whereHas('categorie', function ($query) {
-                $query->whereIn('famille', ['restaurant', 'bar']);
-            })->get();
-
-            // mettre le stock des produits qui ne sont pas dans l'inventaire du mois à 0
-            foreach ($produits_not_inventaire as $produit) {
-                $produit->update([
-                    'stock' => 0,
-                    'stock_dernier_inventaire' => 0,
-                    'stock_initial' => 0,
-                ]);
-            }
-
-            // dd($produits_not_inventaire->toArray());
-
-            ##END
+        //     // recuperer les produits de l'inventaire du mois
+        //     $produits_inventaire = Produit::whereHas('inventaires', function ($query) use ($inventaire_mois) {
+        //         $query->where('inventaires.id', $inventaire_mois->id);
+        //     })->get();
 
 
-        }
+        //     //recuperer les produits où la categorie famille est restaurant et  bar qui ne sont pas dans l'inventaire du mois
+        //     $produits_not_inventaire = Produit::whereDoesntHave('inventaires', function ($query) use ($inventaire_mois) {
+        //         $query->where('inventaires.id', $inventaire_mois->id);
+        //     })->whereHas('categorie', function ($query) {
+        //         $query->whereIn('famille', ['restaurant', 'bar']);
+        //     })->get();
+
+        //     // mettre le stock des produits qui ne sont pas dans l'inventaire du mois à 0
+        //     foreach ($produits_not_inventaire as $produit) {
+        //         $produit->update([
+        //             'stock' => 0,
+        //             'stock_dernier_inventaire' => 0,
+        //             'stock_initial' => 0,
+        //         ]);
+        //     }
+
+        //     // dd($produits_not_inventaire->toArray());
+
+        //     ##END
+
+
+        // }
 
 
 
